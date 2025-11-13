e03c7f2df1aa1df007bed967bb70f69fcd4c05c5
Mauricioam <mauricioam91@gmail.com>
2025-11-13 20:40:28 -0300

    added operate fx, will get the operator string the numbers according to operator will call one of the basics operations

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const add = (num1, num2) => num1 + num2;
 const substract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+let number1 = new Number();
+let number2 = new Number();
+let operator = new String();
+
+const operate = (operator, number1, number2) => {
+  switch (operator) {
+    case "+":
+      add(number1, number2);
+      break;
+    case "-":
+      substract(number1, number2);
+      break;
+    case "*":
+      multiply(number1, number2);
+      break;
+    default:
+      break;
+  }
+};
