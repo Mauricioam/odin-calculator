c2d6bad3d5a1813c864a8528a25050df6b7b8dd3
Mauricioam <mauricioam91@gmail.com>
2025-11-16 17:00:35 -0300

    fixed switch adding a return on every expression

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const add = (num1, num2) => num1 + num2;
-const substract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-
 let number1 = new Number();
 let number2 = new Number();
 let operator = new String();
 
 const operate = (operator, number1, number2) => {
+  const add = (num1, num2) => num1 + num2;
+  const substract = (num1, num2) => num1 - num2;
+  const multiply = (num1, num2) => num1 * num2;
+  const divide = (num1, num2) => num1 / num2;
+
   switch (operator) {
     case "+":
-      add(number1, number2);
-      break;
+      return add(number1, number2);
+
     case "-":
-      substract(number1, number2);
-      break;
+      return substract(number1, number2);
+
     case "*":
-      multiply(number1, number2);
-      break;
+      return multiply(number1, number2);
+
+    case "/":
+      return divide(number1, number2);
+
     default:
+      "Error";
       break;
   }
 };
