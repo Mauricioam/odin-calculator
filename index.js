e044cc905f99b2f60ab64fb4c07840091593fb7f
Mauricioam <mauricioam91@gmail.com>
2025-11-19 22:46:46 -0300

    refactoring fuinctions, to handle the user input. Added handleUserInput

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-let number1 = new Number();
-let number2 = new Number();
-let operator = new String();
+let number1 = "";
+let number2 = "";
+let operator = "";
 
 let btnSelection = document.querySelector(".btn-container");
 let userDisplay = document.querySelector(".user-num");
 let input = document.createElement("p");
 
+const handleNumberInput = (input, num1, num2, operator) => {
+  console.log(num1.length);
+  if (!num1.length) {
+    return (num1 = input);
+  } else {
+    return (num1 += input);
+  }
+};
+
 userDisplay.appendChild(input);
 const btnClicks = (event) => {
   if (event.target.tagName !== "BUTTON") return;
   let userInput = event.target.value;
-  if (!isNaN(userInput)) {
-    if (!number1.length) {
-      number1 = userInput;
-      console.log(operator);
-    } else if (!operator.length) {
-      number1 += userInput;
-    } else if (!number2.length) {
-      number2 = userInput;
-    } else {
-      number2 += userInput;
-    }
-  }
-  if (isNaN(userInput)) {
-    operator = userInput;
-  }
-  if (number2 == 0) {
-    input.textContent = number1 + operator;
-  } else {
-    input.textContent = number1 + operator + number2;
-  }
+  number1 = handleNumberInput(userInput, number1, number2, operator);
+  console.log(number1, "aca");
+  // if (!isNaN(userInput)) {
+  //   if (!number1.length) {
+  //     number1 = userInput;
+  //   } else if (!operator.length) {
+  //     number1 += userInput;
+  //   } else if (!number2.length) {
+  //     number2 = userInput;
+  //   } else {
+  //     number2 += userInput;
+  //   }
+  // }
+  // console.log(operator);
+  // if (isNaN(userInput)) {
+  //   operator = userInput;
+  // }
+  // if (number2 == 0) {
+  //   input.textContent = number1 + operator;
+  // } else {
+  //   input.textContent = number1 + operator + number2;
+  // }
 };
 
 btnSelection.addEventListener("click", btnClicks);
