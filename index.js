e4a5da3efc21ca63cb1eb5c098136c65823da5cd
Mauricioam <mauricioam91@gmail.com>
2025-12-03 21:36:53 -0300

    not atomic, css I finished the sytles adding bg colors. JS, changed the handling of number1

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ let mainDisplay = document.querySelector(".display-num");
 let input = document.createElement("p");
 
 const handleNumberInput = (input) => {
-  if (!number1.length && !isNaN(input)) {
+  if (!number1.length && !isNaN(input) && !operator.length) {
     calcState = "calc1";
-    number1 = input;
-  } else if (!operator.length && !isNaN(input)) {
     number1 += input;
   } else if (number1.length && operator.length && !isNaN(input)) {
     number2 += input;
